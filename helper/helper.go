41c37830a7dcb3292f84cd670294baf3bc92f9e1
titan <titanioy98@gmail.com>
2023-12-10 17:59:04 +0700

    release

--- helper/helper.go
+++ helper/helper.go
@@ -1,11 +1,9 @@
 package helper
 
 import (
-	"log"
 	"os"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/joho/godotenv"
 )
 
 type Response struct {
@@ -46,10 +44,10 @@ func FormatValidationError(err error) []string {
 
 func GoDotEnvVariable(key string) string {
 	// load .env file
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file")
+	// }
 
 	return os.Getenv(key)
 }
